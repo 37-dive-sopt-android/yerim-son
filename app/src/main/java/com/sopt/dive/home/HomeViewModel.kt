60da59c88d6958dc77c2ba5cd1f1789ee4dde537
Yerim Son <sonyerim12@gmail.com>
2025-11-07 16:36:21 +0900

    feat: Home 화면에 MVVM 적용

---
+++ app/src/main/java/com/sopt/dive/home/HomeViewModel.kt
@@ -0,0 +1,38 @@
+package com.sopt.dive.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sopt.dive.util.UserInfo
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+
+class HomeViewModel: ViewModel() {
+
+    private val _profiles = MutableStateFlow<List<Profile>>(emptyList())
+    val profiles: StateFlow<List<Profile>> = _profiles
+
+    private val _userInfo = MutableStateFlow<UserInfo?>(null)
+    val userInfo: StateFlow<UserInfo?> = _userInfo
+
+    init {
+        loadDummyProfiles()
+    }
+
+    fun loadDummyProfiles() {
+        viewModelScope.launch {
+            _profiles.value = List(20) { index ->
+                Profile(
+                    imageUrl = "https://static.megamart.com/product/image/1392/13924003/13924003_1_960.jpg",
+                    name = "안두콩${index + 1}",
+                    description = "콩 ${index + 1}개 굴러가유~"
+                )
+            }
+        }
+    }
+
+    fun loadUserInfo(userInfo: UserInfo) {
+        _userInfo.value = userInfo
+    }
+
+}
