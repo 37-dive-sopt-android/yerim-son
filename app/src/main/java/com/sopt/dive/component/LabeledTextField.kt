7a65119e9e15f79ae492495cce04d713e7c5265a
Yerim Son <sonyerim12@gmail.com>
2025-11-07 15:20:59 +0900

    refactor: PasswordVisualTransformation import 정리

--- app/src/main/java/com/sopt/dive/component/LabeledTextField.kt
+++ app/src/main/java/com/sopt/dive/component/LabeledTextField.kt
@@ -10,6 +10,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.text.input.VisualTransformation
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -41,7 +42,7 @@ fun LabeledTextField(
             .padding(bottom = 20.dp),
         placeholder = { Text(placeholder) },
         singleLine = true,
-        visualTransformation = if (isPassword) PasswordVisualTransformation() else androidx.compose.ui.text.input.VisualTransformation.None
+        visualTransformation = if (isPassword) PasswordVisualTransformation() else VisualTransformation.None
     )
 }
 
