c858163a28d90e57807e57cc14da8311f7060f62
Yerim Son <sonyerim12@gmail.com>
2025-11-12 19:29:30 +0900

    fix: showBottomBar 오류 해결

--- app/src/main/java/com/sopt/dive/MainActivity.kt
+++ app/src/main/java/com/sopt/dive/MainActivity.kt
@@ -1,20 +1,13 @@
 package com.sopt.dive
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.mutableStateOf
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavDestination.Companion.hasRoute
 import com.sopt.dive.ui.theme.DiveTheme
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
@@ -25,17 +18,12 @@ import com.sopt.dive.my.myNavGraph
 import com.sopt.dive.search.searchNavGraph
 import androidx.navigation.compose.NavHost
 import androidx.navigation.navOptions
-import com.sopt.dive.data.ResponseUserListDto
-import com.sopt.dive.data.ServicePool
 import com.sopt.dive.home.navigateToHome
 import com.sopt.dive.login.Login
 import com.sopt.dive.login.loginNavGraph
 import com.sopt.dive.login.navigateToLogin
 import com.sopt.dive.signup.navigateToSignUp
 import com.sopt.dive.signup.signUpNavGraph
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -86,18 +74,14 @@ fun MainScreen() {
     val tabs = MainTab.entries
 
     val currentDestination = navController.currentBackStackEntryAsState().value?.destination
-    val currentRoute = currentDestination?.route ?: MainTab.HOME.route
-    Log.d("MainScreen", "currentRoute = $currentRoute")
 
-    val currentTab = tabs.find { it.route == currentRoute } ?: MainTab.HOME
+    val currentTab = MainTab.find { tab ->
+        currentDestination?.hasRoute(tab.route::class) == true
+    } ?: MainTab.HOME
 
-    val bottomBarRoutes = listOf(
-        MainTab.HOME.route,
-        MainTab.SEARCH.route,
-        MainTab.MYPAGE.route
-    )
-
-    val showBottomBar = bottomBarRoutes.contains(currentRoute)
+    val showBottomBar = MainTab.contains { tab ->
+        currentDestination?.hasRoute(tab.route::class) == true
+    }
 
     Scaffold(
         bottomBar = {
